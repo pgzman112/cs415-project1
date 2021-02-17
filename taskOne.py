efe7da5e029dbda28f14fecaf29944de4bea889c
pgzman112 <pgzman112@gmail.com>
2021-02-17 11:14:10 -0800

    Has some scatter plot stuff doine

--- taskOne.py
+++ taskOne.py
@@ -1,11 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import logging
 import threading
 import time
-import matplotlib.pyplot as plt
-plt.style.use('seaborn-whitegrid')
-import numpy as np
 
-FIBCOUNTER = 1
+FIBCOUNTER = 0
 GCDCOUNTER = 0
 def increment(name):
     if name == fib:
@@ -14,6 +13,11 @@ def increment(name):
     if name == gcd:
         global GCDCOUNTER
         GCDCOUNTER = GCDCOUNTER + 1
+
+def zeroOut(name):
+    if name == fib:
+        global FIBCOUNTER
+        FIBCOUNTER = 0
 
 def fib(k):
     if k == 0:
@@ -47,8 +51,21 @@ if mode == "test":
     print("num of divs made: ", GCDCOUNTER)
 elif mode == "scatter":
     print("Do some stuff")
+    xAxis = np.array(12)
+    yAxis = np.array(12)
+    for i in range (1,30):
+        xAxis = np.append(xAxis, i)
+        temp = fib(i)
+        yAxis = np.append(yAxis, FIBCOUNTER)
+        zeroOut(fib)
+    print(xAxis)
+    print(yAxis)
+    plt.scatter(xAxis, yAxis)
+    plt.show()
+    print("did we finish?")
+
 elif mode == "display":
     for i in range(0,13):
         print(fib(i))
 else:
-    "incorrect input please run the program and try again"
+    print("incorrect input please run the program and try again")
