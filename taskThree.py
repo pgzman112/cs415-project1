6c572b3c37eba9d575029ff7d40ccf7ed7138476
Preston <zimmermp@sonoma.edu>
2021-02-18 16:28:02 -0800

    Good start on part 3

--- taskThree.py
+++ taskThree.py
@@ -1,21 +1,47 @@
 import copy
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import logging
+import threading
+import time
+
+INSCOUNT = 0
+SELCOUNT = 0
+
+def inc(name):
+    if name == "ins":
+        global INSCOUNT
+        INSCOUNT = INSCOUNT + 1
+    if name == 'sel':
+        global SELCOUNT
+        SELCOUNT = SELCOUNT + 1
+
+def zeroOut(name):
+    if name == "ins":
+        global INSCOUNT
+        INSCOUNT = 0
+    if name == 'sel':
+        global SELCOUNT
+        SELCOUNT = 0
 
 def insertionSort(array):
-    start = 0
-    while start < len(array):
-        for i in range(start, 0, -1):
-            if array[i] < array[i-1]:
-                temp = array[i]
-                array[i] = array[i-1]
-                array[i-1] = temp
-        start = start + 1
+    for i in range(1, len(array)):
+        for j in range(i-1, -1, -1):
+            inc('ins')
+            if array[j] > array[j+1]:
+                temp = array[j]
+                array[j] = array[j+1]
+                array[j+1] = temp
+            else:
+                break
     return array
 
 def selectionSort(array):
     for i in range(0, len(array)-1):
         smallest = i
         for j in range(i+1, len(array)):
+            inc('sel')
             if array[smallest] > array[j]:
                 smallest = j
         temp = array[smallest]
@@ -23,30 +49,128 @@ def selectionSort(array):
         array[i] = temp
     return array
 
-fname = 'data/smallSet/data'
-n = 20
-#adding a comment
-temp = np.loadtxt(fname + str(n) + '.txt', dtype = np.int64)
+fname = 'data/testSet/data'
 
-temp = temp.astype(int)
-temp1 = copy.deepcopy(temp)
-print("array unsorted: ")
-for i in temp:
-    print(i, end = ' ')
+xAxis = np.array(0)
+bestCaseInsYAxis = np.array(0)
+avgCaseInsYAxis = np.array(0)
+worstCaseInsYAxis = np.array(0)
+bestCaseSelYAxis = np.array(0)
+avgCaseSelYAxis = np.array(0)
+worstCaseSelYAxis = np.array(0)
 
-print()
-print("sorted array using insertion sort: ")
-t = insertionSort(temp)
-for i in t:
-    print(i, end = ' ')
+for x in range(100, 6000, 500):
+    print("working on data", x)
+    insSortSorted = np.loadtxt(fname + str(x) + '_sorted.txt', dtype=np.int64)  # sorted (best case)
+    insSort = np.loadtxt(fname + str(x) + '.txt', dtype = np.int64) #random data (avg case)
+    insSortReverse = np.loadtxt(fname + str(x) + '_rSorted.txt', dtype = np.int64) #reverse sorted (worst case)
 
-print()
+    insSortSorted = insSortSorted.astype(int)
+    insSort = insSort.astype(int)
+    insSortReverse = insSortReverse.astype(int)
+    selSortSorted = copy.deepcopy(insSortSorted)
+    selSort = copy.deepcopy(insSort)
+    selSortReverse = copy.deepcopy(insSortReverse)
 
-print("array unsorted: ")
-for i in temp1:
-    print(i, end = ' ')
-print()
-print("sorted array using selection sort: ")
-y = selectionSort(temp1)
-for i in y:
-    print(i, end = ' ')
+    #This chunk generates the data for insertion sort on current file
+    insSortSorted = insertionSort(insSortSorted)
+    xAxis = np.append(xAxis, x)
+    bestCaseInsYAxis = np.append(bestCaseInsYAxis, INSCOUNT)
+    print("x: ", x, " num comps for best: ", INSCOUNT)
+    zeroOut('ins')
+    insSort = insertionSort(insSort)
+    avgCaseInsYAxis = np.append(avgCaseInsYAxis, INSCOUNT)
+    print("x: ", x, " num comps for avg: ", INSCOUNT)
+    zeroOut('ins')
+    insSortReverse = insertionSort(insSortReverse)
+    worstCaseInsYAxis = np.append(worstCaseInsYAxis, INSCOUNT)
+    print("x: ", x, " num comps for worst: ", INSCOUNT)
+    zeroOut('ins')
+
+    #This chunk generates the data for selection sort on current file
+    selSortSorted = selectionSort(selSortSorted)
+    bestCaseSelYAxis = np.append(bestCaseSelYAxis, SELCOUNT)
+    print("x: ", x, " num comps for best: ", SELCOUNT)
+    zeroOut('sel')
+    selSort = selectionSort(selSort)
+    avgCaseSelYAxis = np.append(avgCaseSelYAxis, SELCOUNT)
+    print("x: ", x, " num comps for avg: ", SELCOUNT)
+    zeroOut('sel')
+    selSortReverse = selectionSort(selSortReverse)
+    worstCaseSelYAxis = np.append(worstCaseSelYAxis, SELCOUNT)
+    print("x: ", x, " num comps for worst: ", SELCOUNT)
+    zeroOut('sel')
+
+xAxis = np.delete(xAxis, 0)
+bestCaseInsYAxis = np.delete(bestCaseInsYAxis, 0)
+avgCaseInsYAxis = np.delete(avgCaseInsYAxis, 0)
+worstCaseInsYAxis = np.delete(worstCaseInsYAxis,0)
+bestCaseSelYAxis = np.delete(bestCaseSelYAxis, 0)
+avgCaseSelYAxis = np.delete(avgCaseSelYAxis, 0)
+worstCaseSelYAxis = np.delete(worstCaseSelYAxis, 0)
+
+mpl.style.use('seaborn')
+plt.xlim(0, 6000)
+plt.ylim(0, np.amax(bestCaseInsYAxis))
+ax = plt.gca()
+ax.get_xaxis().get_major_formatter().set_scientific(False)
+
+bestCaseInsPlot = plt.figure(1)
+plt.xlabel('Size of n')
+plt.ylabel('comparisons')
+plt.title('Best Case Insertion Sort')
+plt.plot(xAxis, bestCaseInsYAxis, color="green", linewidth=1)
+
+avgCaseInsPlot = plt.figure(2)
+plt.xlim(0, 6000)
+plt.ylim(0, np.amax(worstCaseInsYAxis))
+ax = plt.gca()
+ax.get_xaxis().get_major_formatter().set_scientific(False)
+plt.xlabel('Size of n')
+plt.ylabel('comparisons')
+plt.title('Average Case Insertion Sort')
+plt.plot(xAxis, avgCaseInsYAxis, color="yellow", linewidth=1)
+
+avgCaseInsPlot = plt.figure(3)
+plt.xlim(0, 6000)
+plt.ylim(0, np.amax(worstCaseInsYAxis))
+ax = plt.gca()
+ax.get_xaxis().get_major_formatter().set_scientific(False)
+plt.xlabel('Size of n')
+plt.ylabel('comparisons')
+plt.title('worst Case Insertion Sort')
+plt.plot(xAxis, worstCaseInsYAxis, color="red", linewidth=1)
+
+bestCaseSelPlot = plt.figure(4)
+plt.xlim(0, 6000)
+plt.ylim(0, np.amax(bestCaseSelYAxis))
+ax = plt.gca()
+ax.get_xaxis().get_major_formatter().set_scientific(False)
+plt.xlabel('Size of n')
+plt.ylabel('comparisons')
+plt.title('Best Case Selection Sort')
+plt.plot(xAxis, bestCaseSelYAxis, color="green", linewidth=1)
+
+avgCaseSelPlot = plt.figure(5)
+plt.xlim(0, 6000)
+plt.ylim(0, np.amax(avgCaseSelYAxis))
+ax = plt.gca()
+ax.get_xaxis().get_major_formatter().set_scientific(False)
+plt.xlabel('Size of n')
+plt.ylabel('comparisons')
+plt.title('Average Case Selection Sort')
+plt.plot(xAxis, avgCaseSelYAxis, color="yellow", linewidth=1)
+
+worstCaseSelPlot = plt.figure(6)
+plt.xlim(0, 6000)
+plt.ylim(0, np.amax(worstCaseSelYAxis))
+ax = plt.gca()
+ax.get_xaxis().get_major_formatter().set_scientific(False)
+plt.xlabel('Size of n')
+plt.ylabel('comparisons')
+plt.title('Worst Case Selection Sort')
+plt.plot(xAxis, worstCaseSelYAxis, color="red", linewidth=1)
+
+plt.show()
+
+
--- taskTwo.py
+++ taskTwo.py
@@ -23,34 +23,30 @@ def decByCons(a, n):
         return 1
     if n % 2 == 0:
         inc()
+        temp = decByCons(a, n/2)
+        return temp * temp
+    else:
+        inc()
+        inc()
+        temp = decByCons(a, (n-1)/2)
+        return a * temp * temp
+
+def divAndConq(a,n):
+    if n == 0:
+        return 1
+    if n % 2 == 0:
+        inc()
         return decByCons(a, n/2) * decByCons(a, n/2)
     else:
         inc()
         inc()
         return a * decByCons(a, (n-1)/2) * decByCons(a, (n-1)/2)
 
-def divAndConq(a,n):
-    if a == 0:
-        return 0
-    elif n == 0:
-        return 1
-    elif n < 0:
-        return 0
-    elif n < 1:
-        return 1
-    elif n % 2 == 0:
-        inc()
-        return divAndConq(a*a, n/2)
-    else:
-        inc()
-        inc()
-        return a * divAndConq(a*a, n/2)
-
-print("3 squared using decrease by one = ", decByOne(5,3))
+print("5 to the 6th using decrease by one = ", decByOne(5,6))
 print("number of multiplications done by decrease by one: ", MULTCOUNT)
 zeroOut()
-print("3 squared using decrease by constant = ", decByCons(5,3))
+print("5 to the 6th using decrease by constant = ", decByCons(5,6))
 print("number of multiplications done by decrease by constant: ", MULTCOUNT)
 zeroOut()
-print("3 squared using decrease by constant = ", divAndConq(5,3))
+print("5 to the 6th using decrease by constant = ", divAndConq(5,6))
 print("number of multiplications done by divide and conquer: ", MULTCOUNT)
